90a6989becebc8253a5a64ab43079ba60ff9f9df
pgauna <pgauna@repremar.com>
2024-10-11 16:58:23 -0300

    Commit fin de horario 111024

--- src/componentes/facturacion/comprobantes/Comprobantes.jsx
+++ src/componentes/facturacion/comprobantes/Comprobantes.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import './Comprobantes.css'
 import { Link } from "react-router-dom";
 
 const Comprobantes = ({ isLoggedIn }) => {
@@ -23,57 +23,46 @@ const Comprobantes = ({ isLoggedIn }) => {
   const [eccass, setEcCass] = useState('');
   const [ectipodeembarque, setEcTipoDeEmbarque] = useState('');
   const [ectc, setEcTc] = useState('');
-
-
-
-  const [icnrocheque, setIcNroCheque] = useState('');
-  const [icbanco, setIcBanco] = useState('');
-  const [icfecha, setIcFecha] = useState('');
-  const [ictipoMoneda, setIcTipoMoneda] = useState(false);//*
-  const [icarbitraje, setIcArbitraje] = useState('');
-  const [icimpdelcheque, setIcImpDelCheque] = useState('');
-  const [icimporteendolares, setIcImporteEnDolares] = useState('');
-  const [icfechavencimiento, setIcFechavencimiento] = useState('');
-  const [ictotaldelasguias, setIcTotalDeLasGuias] = useState('');
-  const [ictotalingresado, setIcTotalIngresado] = useState('');
-  const [icsaldodelcheque, setIcSaldoDelCheque] = useState('');
-  const [icimpdeldocumento, setIcImpDelDocumento] = useState('');
-  const [icsaldodeldocumento, setIcSaldoDelDocumento] = useState('');
-  const [iclistadecheques, setIcListaDeCheques] = useState([]);
-
+  const [ecguia, setEcGuia] = useState('');
+  const [ecdescripcion, setEcDescripcion] = useState('');
+  const [ecmonedaguia, setEcMonedaGuia] = useState('');
+  const [ecimporte, setEcImporte] = useState('');
+  const [ectotalacobrar, setEcTotalACobrar] = useState('');
+  const [ecsubtotal, setEcsubtotal] = useState('');
+  const [eciva, setEcIva] = useState('');
+  const [ecredondeo, setEcRedondeo] = useState('');
+  const [ectotal, setEcTotal] = useState('');
+  const [eclistadeguiasasociadas, setEcListaDeGuiasAsociadas] = useState([]);
 
 
   // Estado para la cheque seleccionado
-  const [icchequeseleccionado, setIcChequeSeleccionado] = useState(null);
+  const [ecguiaseleccionada, setEcGuiaSeleccionada] = useState(null);
 
   // Función para seleccionar una factura al hacer clic en una fila
-  const handleSeleccionarChequeLista = (icindex) => {
-    setIcChequeSeleccionado(icindex);
+  const handleSeleccionarGuiaAsociada = (icindex) => {
+    setEcGuiaSeleccionada(icindex);
   };
 
 
 
   // Función para eliminar el cheque seleccionado
-  const handleEliminarChequeCargado = () => {
-    if (icchequeseleccionado !== null) {
+  const handleEliminarGuiaAsociada = () => {
+    if (ecguiaseleccionada !== null) {
       // Filtrar todos los cheques excepto el seleccionado
-      const nuevoschequesseleccionados = iclistadecheques.filter((_, icindex) => icindex !== icchequeseleccionado);
-      setIcListaDeCheques(nuevoschequesseleccionados);
-      setIcChequeSeleccionado(null); // Limpiar la selección
+      const nuevoschequesseleccionados = eclistadeguiasasociadas.filter((_, icindex) => icindex !== ecguiaseleccionada);
+      setEcListaDeGuiasAsociadas(nuevoschequesseleccionados);
+      setEcGuiaSeleccionada(null); // Limpiar la selección
     }
   };
   // Función para agregar una factura asociada a la tabla
-  const handleAgregarChequeCargado = () => {
-    if (icnrocheque && icbanco && icfecha && ictipoMoneda && icimpdelcheque && icfechavencimiento) {
-      const nuevocheque = { icfecha, icbanco, icnrocheque, ictipoMoneda, icimpdelcheque, icfechavencimiento };
-      setIcListaDeCheques([...iclistadecheques, nuevocheque]);
-      setIcNroCheque('');
-      setIcBanco('');
-      setIcTipoMoneda(false);
-      setIcArbitraje('');
-      setIcImpDelCheque('');
-      setIcImporteEnDolares('');
-      setIcFechavencimiento('');
+  const handleAgregarGuiaAsociada = () => {
+    if (ecguia && ecdescripcion && ecmonedaguia && ecimporte) {
+      const nuevaguiaasociada = { ecguia, ecdescripcion, ecmonedaguia, ecmonedaguia, ecimporte };
+      setEcListaDeGuiasAsociadas([...eclistadeguiasasociadas, nuevaguiaasociada]);
+      setEcGuia('');
+      setEcDescripcion('');
+      setEcMonedaGuia('');
+      setEcImporte('');
     }
   };
 
@@ -95,15 +84,15 @@ const Comprobantes = ({ isLoggedIn }) => {
 
 
   return (
-    <div className="IngresarCheque-container">
-      <h2 className='Titulo-ingreso-recibos'>Emisión de Comprobantes</h2>
-      <form onSubmit={handleSubmitAgregarRecibo} className='formulario-emitir-recibo'>
+    <div className="EmitirComprobante-container">
+      <h2 className='titulo-estandar'>Emisión de Comprobantes</h2>
+      <form onSubmit={handleSubmitAgregarRecibo} className='formulario-estandar'>
 
-        <div className='primerafilaingresocheques'>
-          <div className='div-datos-cheque'>
-            <h3 className='Titulos-formularios-ingreso-recibos'>Datos del Comprobante</h3>
+        <div className='primerafilaemisiondecomprobantes'>
+          <div className='div-datos-comprobante'>
+            <h3 className='subtitulo-estandar'>Datos del Comprobante</h3>
 
-            <div className='div-primerrenglon-datos-cheque'>
+            <div className='div-primerrenglon-datos-comprobante'>
               <div>
                 <label htmlFor="ecID">ID:</label>
                 <input
@@ -171,7 +160,7 @@ const Comprobantes = ({ isLoggedIn }) => {
             </div>
 
 
-            <div className='div-primerrenglon-datos-cheque'>
+            <div className='div-primerrenglon-datos-comprobante'>
               <div>
                 <label htmlFor="ecrazonsocial">Razon Social:</label>
                 <input
@@ -242,7 +231,7 @@ const Comprobantes = ({ isLoggedIn }) => {
               </div>
             </div>
 
-            <div className='div-primerrenglon-datos-cheque'>
+            <div className='div-primerrenglon-datos-comprobante'>
               <div>
                 <label htmlFor="ecdireccionfiscal">Dirección Fiscal:</label>
                 <input
@@ -314,116 +303,94 @@ const Comprobantes = ({ isLoggedIn }) => {
                 />
               </div>
             </div>
+          </div>
 
-            
+          <div className='div-guias-asociadas'>
+            <h3 className='subtitulo-estandar'>Guias Asociadas</h3>
+            <div className='div-primerrenglon-datos-comprobante'>
+              <div>
+                <label htmlFor="ecguia">Guia:</label>
+                <input
+                  type="text"
+                  id="ecguia"
+                  value={ecguia}
+                  onChange={(e) => setEcGuia(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="ecdescripcion">Descripción:</label>
+                <input
+                  type="text"
+                  id="ecdescripcion"
+                  value={ecdescripcion}
+                  onChange={(e) => setEcDescripcion(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="ecmonedaguia">Moneda:</label>
+                <select
+                  id="ecmonedaguia"
+                  value={ecmonedaguia}
+                  onChange={(e) => setEcMonedaGuia(e.target.value)}
+                  required
+                >
+                  <option value="">Selecciona una Moneda</option>
+                  <option value="dolares">Dolares</option>
+                  <option value="pesos">Pesos</option>
+                  <option value="Euros">Euros</option>
+                </select>
+              </div>
+              <div>
+                <label htmlFor="ecimporte">Importe:</label>
+                <input
+                  type="text"
+                  id="ecimporte"
+                  value={ecimporte}
+                  onChange={(e) => setEcImporte(e.target.value)}
+                  required
+                />
+              </div>
 
-
-            <div className='div-segundorenglon-datos-cheque'>
               <div className='botonesfacturasasociadas'>
-                <button type="button" className='btn-estandar'>Nuevo</button>
-                <button type="button" className='btn-estandar'>Usar Anterior</button>
-                <button type="button" className='btn-estandar'>Modificar</button>
-                <button type="button" className='btn-eliminar-estandar'>Eliminar</button>
-                <button type="button" onClick={handleAgregarChequeCargado} className='btn-estandar'>Confirmar</button>
+                <button type="button" onClick={handleAgregarGuiaAsociada} className='btn-estandar'>Agregar</button>
               </div>
-            </div>
-
-          </div>
-
-
-
-          <div className='div-totales-cheque'>
-            <h3 className='Titulos-formularios-ingreso-recibos'>Totales del Cheque</h3>
-
-            <div className='div-primerrenglon-datos-recibos'>
-              <div>
-                <label htmlFor="ictotaldelasguias">Total de las Guias:</label>
-                <input
-                  type="text"
-                  id="ictotaldelasguias"
-                  value={ictotaldelasguias}
-                  onChange={(e) => setIcTotalDeLasGuias(e.target.value)}
-                  required
-                />
-              </div>
-              <div>
-                <label htmlFor="ictotalingresado">Total Ingresado:</label>
-                <input
-                  type="text"
-                  id="ictotalingresado"
-                  value={ictotalingresado}
-                  onChange={(e) => setIcTotalIngresado(e.target.value)}
-                  required
-                />
-              </div>
-              <div>
-                <label htmlFor="icsaldodelcheque">Saldo Del Cheque:</label>
-                <input
-                  type="text"
-                  id="icsaldodelcheque"
-                  value={icsaldodelcheque}
-                  onChange={(e) => setIcSaldoDelCheque(e.target.value)}
-                  required
-                />
-              </div>
-              <div>
-                <label htmlFor="icimpdeldocumento">Imp. del Documento:</label>
-                <input
-                  type="text"
-                  id="icimpdeldocumento"
-                  value={icimpdeldocumento}
-                  onChange={(e) => setIcImpDelDocumento(e.target.value)}
-                  required
-                />
-              </div>
-              <div>
-                <label htmlFor="icsaldodeldocumento">Saldo del Documento:</label>
-                <input
-                  type="text"
-                  id="icsaldodeldocumento"
-                  value={icsaldodeldocumento}
-                  onChange={(e) => setIcSaldoDelDocumento(e.target.value)}
-                  required
-                />
-              </div>
-
 
             </div>
           </div>
 
-          <div className='div-tabla-cheque'>
-            <h3 className='Titulos-formularios-ingreso-recibos'>Totales del Cheque</h3>
+          <div className='div-tabla-facturas-asociadas'>
+            <br />
 
             <div className='div-primerrenglon-datos-recibos'>
               {/* Tabla que muestra las facturas agregadas */}
-              <table className='tabla-cheques' >
+              <table className='tabla-guias-asociadas' >
                 <thead>
                   <tr>
-                    <th>Fecha</th>
-                    <th>Banco</th>
-                    <th>Nro. de Cheque</th>
+                    <th>Tipo</th>
+                    <th>Guia</th>
+                    <th>Descripción</th>
                     <th>Moneda</th>
                     <th>Importe</th>
-                    <th>Vencimiento</th>
                     <th>Acciones</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {iclistadecheques.map((cheque, indexic) => (
+                  {eclistadeguiasasociadas.map((guia, indexec) => (
                     <tr
-                      key={indexic}
-                      onClick={() => handleSeleccionarChequeLista(indexic)}
+                      key={indexec}
+                      onClick={() => handleSeleccionarGuiaAsociada(indexec)}
                       style={{
                         cursor: 'pointer' // Indica que la fila es clickeable
                       }}
                     >
-                      <td>{cheque.icfecha}</td>
-                      <td>{cheque.icbanco}</td>
-                      <td>{cheque.icnrocheque}</td>
-                      <td>{cheque.ictipoMoneda}</td>
-                      <td>{cheque.icimpdelcheque}</td>
-                      <td>{cheque.icfechavencimiento}</td>
-                      <td><button type="button" className="action-button" onClick={handleEliminarChequeCargado} disabled={icchequeseleccionado !== indexic}>❌</button></td>
+                      <td>{guia.ectipo}</td>
+                      <td>{guia.ecguia}</td>
+                      <td>{guia.ecdescripcion}</td>
+                      <td>{guia.ecmonedaguia}</td>
+                      <td>{guia.ecimporte}</td>
+                      <td><button type="button" className="action-button" onClick={handleEliminarGuiaAsociada} disabled={ecguiaseleccionada !== indexec}>❌</button></td>
                     </tr>
                   ))}
                 </tbody>
@@ -431,13 +398,72 @@ const Comprobantes = ({ isLoggedIn }) => {
 
 
             </div>
+
+
           </div>
+          <div className='div-totales-comprobante'>
+            <h3 className='subtitulo-estandar'>Totales</h3>
+            <div className='div-primerrenglon-datos-comprobante'>
+
+              <div>
+                <label htmlFor="ecsubtotal">Subtotal:</label>
+                <input
+                  type="text"
+                  id="ecsubtotal"
+                  value={ecsubtotal}
+                  onChange={(e) => setEcsubtotal(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="ecivatotal">IVA:</label>
+                <input
+                  type="text"
+                  id="ecivatotal"
+                  value={eciva}
+                  onChange={(e) => setEcIva(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="ecredondeo">Redondeo:</label>
+                <input
+                  type="text"
+                  id="ecredondeo"
+                  value={ecredondeo}
+                  onChange={(e) => setEcRedondeo(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="ectotal">Total:</label>
+                <input
+                  type="text"
+                  id="ectotal"
+                  value={ectotal}
+                  onChange={(e) => setEcTotal(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="ectotalacobrar">Total a Cobrar:</label>
+                <input
+                  type="text"
+                  id="ectotalacobrar"
+                  value={ectotalacobrar}
+                  onChange={(e) => setEcTotalACobrar(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+          </div>
+
 
         </div>
 
 
 
-        <div className='botonesagregarcheque'>
+        <div className='botonesemitircomprobante'>
           <button type="submit" className='btn-estandar'>Confirmar</button>
 
           <Link to="/home"><button className="btn-estandar">Volver</button></Link>
