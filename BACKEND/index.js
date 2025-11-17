35ecd6277902c96f3401e1837a15e292edcebb2e
pgauna <pgauna@repremar.com>
2025-11-17 16:05:18 -0300

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -82,9 +82,6 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
 
 app.use(cors());
 cron.schedule('34 14 * * *', async () => {
